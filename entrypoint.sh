97ed48bb56f618672cc68e4d2e6f0bef3d8b45bf
dingdayu <614422099@qq.com>
2019-01-02 17:42:20 +0800

    fix entrypoint: init.lock
    Signed-off-by: dingdayu <614422099@qq.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-if [ ! -e /yapi/log/init.lock ]; then
+if [ ! -e /yapi/init.lock ]; then
     npm run install-server
-else
-    touch /yapi/log/init.lock
-    npm run start
 fi
+
+npm run start
